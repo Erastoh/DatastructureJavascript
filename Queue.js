eddc476c39d300bfe783c19359292dd7a566cfde
njogu99 <nnjogu49@gmail.com>
2023-07-28 08:10:23 +0300

    queue optimized

--- Queue.js
+++ Queue.js
@@ -55,3 +55,57 @@ console.log(que.dequeue()) //20 - remove the first element on the que
 console.log(que.peek())  // 10 is in the front of the que.
 
 
+
+//Optimized Queue 
+//the engueue has constant time complexity compared to previous logics
+
+
+class Queue {
+    constructor () {
+        this.items = {}
+        this.front = 0
+        this.rear = 0
+    }
+    enqueue(element) {
+        this.items[this.rear] = element
+        this.rear++
+    }
+    dequeue(element) {
+        const item = this.items[this.front]
+        delete this.items[this.front]
+        this.front++
+        return item
+    }
+
+    isEmpty() {
+        return this.items.rear - this.front === 0
+    }
+
+    peek() {
+        //return the front element withought deliting it.
+        return this.items[this.front]
+    }
+
+    size() {
+        return this.rear - this.front
+    }
+
+    print() {
+        console.log(this.items)
+    }
+
+}
+
+const que2 = new Queue()
+console.log(que2.isEmpty())
+que2.enqueue(20)
+que2.enqueue(10)
+que2.enqueue(30)
+console.log(que2.size())
+que2.print() // 20,10,30
+console.log(que2.dequeue()) //20 - remove the first element on the que
+console.log(que2.peek())  // 10 is in the front of the que.
+
+
+
+
