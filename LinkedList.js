4c528ada9f95c4b749cc23e05ee155689eb0a847
Erastoh <erastudmugambi@gmail.com>
2023-08-08 10:55:32 +0300

    Linked List Intro

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,4 @@
 // a linked list is a linear data structure that includes a series of connected nodes 
 //Each Node consists of a data value and a pointer that points to the next node
 // The list element can be easily inserted or removed withought reallocation or reorganization of the entire structure
+
