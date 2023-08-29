2e24f5aeb3263b4bb144b617a708fc39d7c1e0b2
Erastoh <erastudmugambi@gmail.com>
2023-08-29 21:10:36 +0300

    Linked List Insert When list is empty

--- LinkedList.js
+++ LinkedList.js
@@ -32,7 +32,7 @@ class LinkedList {
 	getSize() {
 		return this.size
 	}
-// Prepend has a constant time complexity 0(1) because it does not depend on how many items are there in the list
+	// Prepend has a constant time complexity 0(1) because it does not depend on how many items are there in the list
 	prepend(value) {
 		const node = new Node(value)
 		if (this.isEmpty()) {
@@ -46,6 +46,7 @@ class LinkedList {
 
 	//Insert an item on a linked list
 
+
 	insert(value, index) {
 		//handling invalid index
 		if (index < 0 || index > this.size) {
@@ -54,11 +55,21 @@ class LinkedList {
 		//adding from an empty list 
 		if (index === 0) {
 			this.prepend(value)
+		} else {
+			//adding when
+			const node = new Node()
+			let prev = this.head
+			for (let i = 0; i < index.lenght; i++) {
+				prev = prev.next
+			}
+			node.next = prev.next
+			prev.next = node
+			this.size++
 		}
 	}
 
 
-//Append had a liniear time complexity 0(n) because it depends on the list of items in the node
+	//Append had a liniear time complexity 0(n) because it depends on the list of items in the node
 	append(value) {
 		const node = new Node(value)
 		if (this.isEmpty()) {
@@ -106,3 +117,7 @@ list.append(60)
 list.append(70)
 list.print() // will print 50 60 70 
 // full list printed 10 20 30 40 80 90 100 50 60 70 
+
+
+list.insert(200, 6)
+list.print()
