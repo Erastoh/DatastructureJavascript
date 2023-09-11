dd47850df4af2b31be8a514bb63e2ce242e3f7e4
Erastoh <erastudmugambi@gmail.com>
2023-09-11 11:01:15 +0300

    Remove a node From Linkedlist

--- LinkedList.js
+++ LinkedList.js
@@ -11,12 +11,12 @@
 
 
 
+
 class Node {
 	constructor (value) {
 		this.value = value
 		this.next = null
 	}
-
 }
 
 class LinkedList {
@@ -32,7 +32,6 @@ class LinkedList {
 	getSize() {
 		return this.size
 	}
-	// Prepend has a constant time complexity 0(1) because it does not depend on how many items are there in the list
 	prepend(value) {
 		const node = new Node(value)
 		if (this.isEmpty()) {
@@ -44,38 +43,6 @@ class LinkedList {
 		this.size++
 	}
 
-	//Insert an item on a linked list
-
-
-	insert(value, index) {
-		//handling invalid index
-		if (index < 0 || index > this.size) {
-			return
-		}
-		//adding from an empty list 
-		if (index === 0) {
-			this.prepend(value)
-		} else {
-			//adding when
-			const node = new Node(value)
-			let prev = this.head
-			for (let i = 0; i < index.lenght; i++) {
-				prev = prev.next
-			}
-			node.next = prev.next
-			prev.next = node
-			this.size++
-		}
-	}
-
-	removeFrom(index) {
-		if (index > 0 || index >= this.size) {
-			return null
-		}
-	}
-
-
-	//Append had a liniear time complexity 0(n) because it depends on the list of items in the node
 	append(value) {
 		const node = new Node(value)
 		if (this.isEmpty()) {
@@ -89,6 +56,49 @@ class LinkedList {
 		}
 		this.size++
 	}
+
+	insert(value, index) {
+		//handling invalid index
+		if (index < 0 || index > this.size) {
+			return null
+		}
+		//adding from an empty list 
+		if (index === 0) {
+			this.prepend(value)
+		} else {
+			//adding when
+			const node = new Node(value)
+			let prev = this.head
+			for (let i = 0; i < index - 1; i++) {
+				prev = prev.next
+			}
+			node.next = prev.next
+			prev.next = node
+			this.size++
+		}
+	}
+
+	removeFrom(index) {
+		if (index < 0 || index >= this.size) {
+			return null
+		}
+		//remove index = 0
+		let removedNode
+		if (index === 0) {
+			removedNode = this.head
+			this.head = this.head.next
+		} else {
+			let prev = this.head
+			for (let i = 0; i < index - 1; i++) {
+				prev = prev.next
+			}
+			removedNode = prev.next
+			prev.next = removedNode.next
+		}
+		this.size--
+		return removedNode.value
+	}
+
 
 
 	print() {
@@ -112,18 +122,26 @@ list.print()
 list.prepend(10)
 list.prepend(20)
 list.prepend(30)
-list.prepend(40)
-list.prepend(80)
-list.prepend(90)
-list.prepend(100)
-list.print() // will print 10 20 30 40 80 90 100
+list.print()
 
 list.append(50)
 list.append(60)
 list.append(70)
-list.print() // will print 50 60 70 
-// full list printed 10 20 30 40 80 90 100 50 60 70 
-
-
-list.insert(200, 6)
 list.print()
+
+console.log('List is Insert: ', list.insert(80, 3))
+//   Node {
+//   value: 30,
+//   next: Node { value: 80, next: Node { value: 20, next: [Node] } }
+// }
+list.print()
+console.log('List is remove: ', list.removeFrom(2))
+list.print()
+list.print(list.getSize())
+
+
+
+
+
+
+
