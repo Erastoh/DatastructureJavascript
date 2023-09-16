6504e75b56ad3132e01c100b24faecbb4eab0e1d
Erastoh <erastudmugambi@gmail.com>
2023-09-16 13:00:06 +0300

    LinkedList with Tail Remove From Tail

--- LinkedList.js
+++ LinkedList.js
@@ -1,16 +1,3 @@
-// A linked list is a linear data structure that includes a series of connected nodes 
-// Each Node consists of a data value and a pointer that points to the next node
-// The list element can be easily inserted or removed withought reallocation or reorganization of the entire structure
-// Random access of elements is not feasible and accessing an element has linear time complexity. 
-
-// The linked list data structure supports three main operations
-// - Insert- to add an element at the beginning, end or at a given index in the list
-// - Deletion - to remove an item given its index or value
-// - Search - to find an element given its value
-
-
-
-
 
 class Node {
 	constructor (value) {
@@ -22,6 +9,7 @@ class Node {
 class LinkedList {
 	constructor (value) {
 		this.head = null
+		this.tail = null
 		this.size = 0 //to track the number of nodes
 	}
 
@@ -31,127 +19,6 @@ class LinkedList {
 
 	getSize() {
 		return this.size
-	}
-	prepend(value) {
-		const node = new Node(value)
-		if (this.isEmpty()) {
-			this.head = node
-		} else {
-			node.next = this.head
-			this.head = node
-		}
-		this.size++
-	}
-
-	append(value) {
-		const node = new Node(value)
-		if (this.isEmpty()) {
-			this.head = node
-		} else {
-			let prev = this.head
-			while (prev.next) {
-				prev = prev.next
-			}
-			prev.next = node
-		}
-		this.size++
-	}
-
-	insert(value, index) {
-		//handling invalid index
-		if (index < 0 || index > this.size) {
-			return null
-		}
-		//adding from an empty list 
-		if (index === 0) {
-			this.prepend(value)
-		} else {
-			//adding when
-			const node = new Node(value)
-			let prev = this.head
-			for (let i = 0; i < index - 1; i++) {
-				prev = prev.next
-			}
-			node.next = prev.next
-			prev.next = node
-			this.size++
-		}
-	}
-
-	removeFrom(index) {
-		if (index < 0 || index >= this.size) {
-			return null
-		}
-		//remove index = 0
-		let removedNode
-		if (index === 0) {
-			removedNode = this.head
-			this.head = this.head.next
-		} else {
-			let prev = this.head
-			for (let i = 0; i < index - 1; i++) {
-				prev = prev.next
-			}
-			removedNode = prev.next
-			prev.next = removedNode.next
-		}
-		this.size--
-		return removedNode.value
-	}
-//removing a value From Linked list// ---- has a linear time complexity. 
-	removeValue(value) {
-		if (this.isEmpty()) {
-			return null
-		}
-		if (this.head.value === value) {
-			this.head = this.head.next
-			this.size--
-			return value
-		} else {
-			let prev = this.head
-			while (prev.next && prev.next.value !== value) {
-				prev = prev.next
-			}
-			if (prev.next) {
-				const removedNode = prev.next
-				prev.next = removedNode.next
-				this.size--
-				return value
-			}
-			return null
-		}
-	}
-
-	search(value) {
-		if (this.isEmpty()) {
-			return -1
-		}
-		let i = 0
-		let curr = this.head
-		while (curr) {
-			if (curr.value === value) {
-				return i
-			}
-			curr = curr.next
-			i++
-		}
-		return -1
-	}
-
-	reverse() {
-		let prev = null
-		let curr = this.head
-		while (curr) {
-			let next = curr.next
-			console.log('List is next: ', next)
-			curr.next = prev
-			console.log('List is curr next: ', curr.next)
-			prev = curr
-			console.log('List is prev: ', prev)
-			curr = next
-			console.log('List is curr: ', curr)
-		}
-		this.head = prev
 	}
 
 	print() {
@@ -167,40 +34,81 @@ class LinkedList {
 			console.log('List Length:', listlength)
 		}
 	}
+
+	prepend(value) {
+		const node = new Node(value)
+		if (this.isEmpty()) {
+			this.head = node
+			this.tail = node
+		} else {
+			node.next = this.head
+			this.head = node
+		}
+		this.size++
+
+	}
+
+	append(value) {
+		const node = new Node(value)
+		if (this.isEmpty()) {
+			this.head = node
+			this.tail = node
+		} else {
+			node.next = this.tail
+			this.tail = node
+		}
+		this.size++
+	}
+
+	//has Constant time comlexity 0(1)
+	removeFromFront() {
+		if (this.isEmpty()) {
+			return null
+		}
+		const value = this.head.value
+		this.head = this.head.next
+		this.size--
+		return value
+	}
+
+	// has linear time Complexity 0(n)
+	removeFromEnd() {
+		if (this.isEmpty()) {
+			return null
+		}
+		const value = this.tail.value
+		if (this.size === 1) {
+			this.head = null
+			this.tail = null
+		} else {
+			let prev = this.head
+			while (prev.next !== this.tail) {
+				prev = prev.next
+			}
+			prev.next = null
+			this.tail = prev
+		}
+		this.size--
+		return value
+	}
+
+
 }
 const list = new LinkedList
 console.log('List is empty: ', list.isEmpty()) // List is empty:  true
-console.log('List is Size: ', list.getSize()) //  List is Size:  0
-list.print()
-list.prepend(10)
-list.prepend(20)
-list.prepend(30)
-list.print()
+console.log('List Size: ', list.getSize()) //  List is Size:  0
 
-list.append(50)
-list.append(60)
-list.append(70)
+list.append(1)
+list.append(2)
+list.append(3)
+list.prepend(0)
 list.print()
+console.log('List Size: ', list.getSize())
 
-console.log('List is Insert: ', list.insert(80, 3))
-//   Node {
-//   value: 30,
-//   next: Node { value: 80, next: Node { value: 20, next: [Node] } }
-// }
-list.print()
-console.log('List is remove: ', list.removeFrom(2))
-list.print()
-list.print(list.getSize())
-
-console.log('List is remove by value: ', list.removeValue(50)) // 30 20 80 60 70 
+list.removeFromFront()
 list.print()
 
-console.log('List is Search: ', list.search(70)) // 4
+list.removeFromEnd()
 list.print()
-
-
-
-
-
 
 
