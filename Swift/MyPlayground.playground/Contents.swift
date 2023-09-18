cff6be70cf5949914ee06d71b9cbf4537562a765
Erastoh <erastudmugambi@gmail.com>
2023-09-18 10:35:03 +0300

    Swift Classes and Struct

--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -15,6 +15,53 @@ func isEven(number: Int) ->Bool{
 var x = 4
 var y = 9
 let sum = addTwoNumbers(a: x, b: y)
-isEven(number: 100)
+let result = isEven(number: 100)
 isEven(number:37)
 isEven(number:193)
+
+
+//Classes And Structs
+//Class is a reference type
+class Car{
+    let color: String
+    let make: String
+    //init is a constructor of the class.
+    init(color: String, make: String){
+        self.color = color
+        self.make = make
+    }
+    
+}
+
+var bmw:Car = Car(color: "Black", make: "BMW")
+var Honda = Car(color: "White", make: "Honda")
+var Subaru = Honda
+
+//Struct
+//Struct is a value type.
+struct Car1{
+    let color: String
+    let make: String
+    //init is a constructor of the class.
+    init(color: String, make: String){
+        self.color = color
+        self.make = make
+    }
+    
+}
+
+var bmw2:Car1 = Car1(color: "Black", make: "BMW")
+var Honda2 = Car1(color: "White", make: "Honda")
+var Subaru2 = Honda2 //Subaru2 | Honda2 , is not dependent on.
+
+//Loops
+
+
+
+
+
+
+
+
+
+
