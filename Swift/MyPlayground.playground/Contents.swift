e6f9ef8636751fa6d6b50e4970de812733186cbb
Erastoh <erastudmugambi@gmail.com>
2023-09-18 09:57:54 +0300

    Switft isEven Number

--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -5,8 +5,16 @@ func addTwoNumbers(a:Int, b:Int) ->Int{
     return a+b
 }
 
+func isEven(number: Int) ->Bool{
+    if number % 2 == 0{
+        return true
+    }
+    return false
+}
+
 var x = 4
 var y = 9
 let sum = addTwoNumbers(a: x, b: y)
-
-
+isEven(number: 100)
+isEven(number:37)
+isEven(number:193)
