fc01bad31c7bc035396889f239a4aefa1e2b16b1
Erastoh <erastudmugambi@gmail.com>
2023-09-23 11:13:52 +0300

    LinkedList Queue

--- LinkedlistQueue.js
+++ LinkedlistQueue.js
@@ -6,21 +6,32 @@ class LinkedListQueue {
 	}
 
 	enqueue(value) {
-
+		this.list.append(value)
 	}
 	dequeue() {
-
+		return this.list.removeFromFront()
 	}
 	peek() {
-
+		return this.list.head.value
 	}
 	isEmpty() {
-
+		return this.list.isEmpty()
 	}
 	getSize() {
-
+		return this.list.getSize()
 	}
 	print() {
-
+		return this.list.print()
 	}
 }
+
+const queue = new LinkedListQueue()
+console.log(queue.isEmpty())
+console.log(queue.getSize())
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.getSize())
+console.log(queue.peek())
+console.log(queue.dequeue())
+queue.print()
