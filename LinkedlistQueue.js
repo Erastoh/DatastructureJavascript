ae77aa280d9e1cf603f8ebfb1d922511e9165024
Erastoh <erastudmugambi@gmail.com>
2023-09-23 10:57:30 +0300

    LinkedList Queue

---
+++ LinkedlistQueue.js
@@ -0,0 +1,26 @@
+const LinkedList = require('./LinkedListWithTail')
+
+class LinkedListQueue {
+	constructor () {
+		this.list = new LinkedList()
+	}
+
+	enqueue(value) {
+
+	}
+	dequeue() {
+
+	}
+	peek() {
+
+	}
+	isEmpty() {
+
+	}
+	getSize() {
+
+	}
+	print() {
+
+	}
+}
