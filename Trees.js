dabe7500a5481134ad087a43b1743386a1babb1b
Erastoh <erastudmugambi@gmail.com>
2023-09-26 07:50:52 +0300

    Depth First Search, pre, in , post Order algorithms

--- Trees.js
+++ Trees.js
@@ -1,6 +1,5 @@
 //A Tree is a hierachical datastructure that consists of Nodes connected by Edges. It is a non-linear
 //Binary Tree is a tree data Structure in which each node has atleast 2 childeren
-
 class Node {
 	constructor (value) {
 		this.value = value
@@ -56,7 +55,6 @@ class BinarySearchTree {
 			}
 		}
 	}
-
 	// PreOrder Traversal
 	//Depth First Search
 	preorder(root) {
@@ -66,9 +64,25 @@ class BinarySearchTree {
 			this.preorder(root.right)
 		}
 	}
+	inOrder(root) {
+		if (root) {
+			this.inOrder(root.left)
+			console.log(root.value)
+			this.inOrder(root.right)
+		}
+	}
 
-  //InOrder Traversal
+	postOrder(root) {
+		if (root) {
+			this.postOrder(root.left)
+			this.postOrder(root.right)
+			console.log(root.value)
+		}
+	}
 
+
+
+	//InOrder Traversal
 }
 
 const bst = new BinarySearchTree()
@@ -77,9 +91,14 @@ console.log("Binary Tree is Empty,", bst.isEmpty())
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+bst.insert(3)
+bst.insert(7)
 
 console.log("Binary Search,", bst.search(bst.root, 10))
 console.log("Binary Search,", bst.search(bst.root, 5))
 console.log("Binary Search,", bst.search(bst.root, 15))
 bst.preorder(bst.root)
-//Tree Traversal
+console.log("InOrder Search")
+bst.inOrder(bst.root)
+console.log("PostOrder Search")
+bst.postOrder(bst.root)
