e1f4a4bb49c55be57c2b6a4aab48309fdd9198a8
Erastoh <erastudmugambi@gmail.com>
2024-01-02 07:53:51 +0300

    Maximum Number

---
+++ InterViewQuestion/GeneralQuestions.js
@@ -0,0 +1,20 @@
+//Write a JavaScript program to find the maximum number in an array
+
+// solution 
+
+// function MaximumNumber(arr) {
+// 	let maximumnum = 1;
+// 	for (let i = 0; i < arr.length; i++){
+// 		if (arr[i] > ) {
+			
+// 		}
+// 	}
+	
+// }
+
+// const arr = [2,4,5,69,9,7]
+
+function MaximumNumber(arr) {
+	return print(Math.max(arr));
+}
+const arr = [2, 4, 5, 69, 9, 7]
