79ee4fccf4b6243242a7827a0c6f89a872f9bae2
Erastoh <erastudmugambi@gmail.com>
2024-10-26 13:41:00 +0300

    Two Sum other fomular

--- InterViewQuestion/quizz_answers/twosum.js
+++ InterViewQuestion/quizz_answers/twosum.js
@@ -1,12 +1,30 @@
 // Given an array of integers nums and an integer target
 //return incdices of the two numbers such that they add up tp target
 
-const twoSum = function (nums, target) {
+// const twoSum = function (nums, target) {
+// 	for (let i = 0; i < nums.length; i++){
+// 		for (let j = i+1; j < nums.length; j++) {
+// 			if(nums[i] + nums[j] == target) return [i,j]
+// 		}
+// 	}
+// }
+
+// console.log(twoSum([2, 2, 6, 3, 8], 9));
+
+const twosum2 = function (nums, target) {
+	var obj = {};
+
 	for (let i = 0; i < nums.length; i++){
-		for (let j = i+1; j < nums.length; j++) {
-			if(nums[i] + nums[j] == target) return [i,j]
+		var n = nums[i];
+		
+		console.log(n);
+		
+		if (obj[target - n] >= 0) {
+			return [obj[target - n], i];
+		} else {
+			obj[n] = i;
 		}
 	}
 }
 
-console.log(twoSum([2, 2, 6, 3, 8], 9));
+console.log(twosum2([2, 8, 6, 4, 1], 9));
