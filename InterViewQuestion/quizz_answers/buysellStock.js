26e94f10d40d3aa64f20443e98307b6a734b3266
Erastoh <erastudmugambi@gmail.com>
2024-10-28 13:17:48 +0300

    Stocks time to Sell meth 2

--- InterViewQuestion/quizz_answers/buysellStock.js
+++ InterViewQuestion/quizz_answers/buysellStock.js
@@ -6,6 +6,8 @@
 // input: prices = [7,1,5,3,6,4]; ---->>>> output:5;
 // input: prices = [7,6,4,3,1];---->>>> output:0;
 
+
+//Brute Force solution
 function maxProfit(prices) {
 	let globalProfit = 0;
 	for (let i = 0; i < prices.length - 1; i++){
@@ -21,3 +23,15 @@ function maxProfit(prices) {
 
 console.log(maxProfit([7, 6, 4, 3, 1]));
 
+//Greedy Algorithmns
+
+const maxProfit = function (prices) {
+	let minStockPurchasePrice = prices[0] || 0;
+	let profit = 0;
+
+	for (let i = 1; i < prices.length; i++){
+		
+	}
+
+
+}
