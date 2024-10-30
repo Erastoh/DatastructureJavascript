fa397ef2da05e11c2f37bf1dde9ed859e2bbb50b
Erastoh <erastudmugambi@gmail.com>
2024-10-30 17:08:27 +0300

    sum of two nums

--- InterViewQuestion/quizz_answers/twosum.js
+++ InterViewQuestion/quizz_answers/twosum.js
@@ -2,26 +2,26 @@
 //return incdices of the two numbers such that they add up tp target
 
 const twoSum = function (nums, target) {
-	for (let i = 0; i < nums.length; i++){
-		for (let j = i+1; j < nums.length; j++) {
-			if(nums[i] + nums[j] == target) return [i,j]
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] == target) return [i, j]
 		}
 	}
 }
 
-console.log(twoSum([2, 2, 6, 3, 8], 9));
+// console.log(twoSum([2, 2, 6, 3, 8], 9));
 
 const twosum2 = function (nums, target) {
 	var obj = {};
 
-	for (let i = 0; i < nums.length; i++){
+	for (let i = 0; i < nums.length; i++) {
 		var n = nums[i];
 		if (obj[target - n] >= 0) {
 			return [obj[target - n], i];
 		} else {
-			return obj[n] = i;
+			obj[n] = i;
 		}
 	}
 }
 
-console.log(twosum2([2, 10, 6, 1, 9,4], 9));
+console.log(twosum2([2, 1, 6, 8, 4], 9));
