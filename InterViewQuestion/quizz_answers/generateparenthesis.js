59ce4e3e8125e62b2c24a18d2bfe9bfd004bb84b
Erastoh <erastudmugambi@gmail.com>
2024-11-12 08:36:36 +0300

    generate parenthesis

--- InterViewQuestion/quizz_answers/generateparenthesis.js
+++ InterViewQuestion/quizz_answers/generateparenthesis.js
@@ -1,10 +1,19 @@
 const parenthesis = function () {
 
-	let setparenthesis = [
-		"((()))",
-		"(()())",
-	];
+	// let setparenthesis = [
+	// 	"((()))",
+	// 	"(()())",
+	// ];
 
-		
-	
+	const solution = [];
+
+	const generateCombo = (leftPCount, rightPCount, partial) => {
+		if (leftPCount > rightPCount) return;
+		if (!leftPCount && !rightPCount) solution.push(partial);
+
+	}
+
+	generateCombo(n, n, "");
+	return solution;
+
 }
