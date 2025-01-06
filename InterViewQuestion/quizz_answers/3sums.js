3859f4101b18af1c14c0994b98fc40076ea185c9
Erastoh <erastudmugambi@gmail.com>
2025-01-06 11:03:38 +0300

    3 sum

--- InterViewQuestion/quizz_answers/3sums.js
+++ InterViewQuestion/quizz_answers/3sums.js
@@ -1,11 +1,6 @@
 //Given an array of n integers, are there elements a, b,c in nums such that a + b + c = 0? find all uniques triplets in the array which gives the sum of zero. 
 //Note : The solution set must not contain duplicates tripplets. 
 
-
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
 var threeSum = function (nums) {
 	let [solution, left, right] = [[], 0, nums.length - 1];
 	if (nums.length < 3) return solution;
