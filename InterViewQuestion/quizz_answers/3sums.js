be07123af2f56f8aa9c8a5d2422deadc40769b45
Erastoh <erastudmugambi@gmail.com>
2025-09-20 07:15:24 +0300

    AI interview questions

--- InterViewQuestion/quizz_answers/3sums.js
+++ InterViewQuestion/quizz_answers/3sums.js
@@ -36,3 +36,7 @@ var threeSum = function (nums) {
 	}
 	return solution;
 }
+
+// Question 2
+// Given an integer array nums, return all the triplets[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
